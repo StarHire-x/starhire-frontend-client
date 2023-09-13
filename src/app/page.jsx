29df57e2b062b0648b4238cd86cf986982e014ef
rilwan99 <rilwanwild@gmail.com>
2023-09-13 15:48:27 +0800

    update home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,8 +7,10 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
-        <h1 className={styles.title}>Better design for your digital products.</h1>
-        <p className={styles.desc}>Turning your ideas into Reality. We bring together the teams from the global tech industry.</p>
+        <h1 className={styles.title}>Early Childhood Industry.</h1>
+        <p className={styles.desc}>Candidate Sourcing</p>
+        <p className={styles.desc}>Job Matching</p>
+        <p className={styles.desc}>An All-in-one Platform</p>
         <Button url="/portfolio" text="See Our Works"/>
       </div>
       <div className={styles.item}></div>
