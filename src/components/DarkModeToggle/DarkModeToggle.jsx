dbfb1499ab384d6379ca1f1cb6fcd720cd90f2a6
hengyjj <hengyjj@hotmail.com>
2023-09-14 20:15:14 +0800

    updated navbar dark/light mode button

--- src/components/DarkModeToggle/DarkModeToggle.jsx
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -10,7 +10,7 @@ const DarkModeToggle = () => {
     <div className={styles.container} onClick={toggle}>
         <div className={styles.icon}>🌙</div>
         <div className={styles.icon}>🔆</div>
-        <div className={styles.ball} style={mode === "light" ? {left: "2px"}: {right: "2px"}}/>
+        <div className={styles.ball} style={mode === "dark" ? {left: "2px"}: {right: "2px"}}/>
     </div>
   )
 }
