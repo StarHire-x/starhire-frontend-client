9d1a8378e1eec07ecd66a08e3223de9f0fc3cf2b
rilwan99 <rilwanwild@gmail.com>
2023-09-14 22:42:41 +0800

    Refactor navbar for mobile UI

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,89 +5,88 @@ import styles from "./navbar.module.css";
 import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 import { signOut, useSession } from "next-auth/react";
 import { useState } from "react";
+import NavItem from "./NavItem";
 
 const links = [
   {
     id: 1,
-    title: 'Home',
-    url: '/',
+    title: "Home",
+    url: "/",
   },
   {
     id: 2,
-    title: 'Portfolio',
-    url: '/portfolio',
+    title: "Portfolio",
+    url: "/portfolio",
   },
   {
     id: 3,
-    title: 'Manage Job Listings',
-    url: '/jobListingManagement',
+    title: "Manage Job Listings",
+    url: "/jobListingManagement",
   },
   {
     id: 4,
-    title: 'Blog',
-    url: '/blog',
+    title: "Blog",
+    url: "/blog",
   },
   {
-    id: 3,
+    id: 5,
     title: "Contact",
     url: "/contact",
   },
   {
-    id: 4,
+    id: 6,
     title: "Dashboard",
     url: "/dashboard",
   },
   {
-    id: 5,
+    id: 7,
     title: "Chat",
     url: "/chat",
   },
 ];
 
+const MENU_LIST = [
+  { text: "Home", href: "/" },
+  { text: "Portfolio", href: "/portfolio" },
+  { text: "Job Listings", href: "/jobListings" },
+  { text: "Blog", href: "/blog" },
+  { text: "Contact", href: "/contact" },
+  { text: "Chat", href: "/chat" },
+];
+
 const Navbar = () => {
   const session = useSession();  
-  const [showDropdown, setShowDropdown] = useState(null);
-
-  const handleLinkMouseEnter = (linkId) => {
-    setShowDropdown(linkId);
-  };
-
-  const handleLinkMouseLeave = () => {
-    setShowDropdown(null);
-  };
+  
+  const [navActive, setNavActive] = useState(null);
+  const [activeIdx, setActiveIdx] = useState(-1);
 
   return (
-    <div className={styles.container}>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
       <Link href="/" className={styles.logo}>
         StarHire
       </Link>
-      <div className={styles.links}>
-        <DarkModeToggle />
-        {links.map((link) => (
-          <div
-            key={link.id}
-            className={styles.linkContainer}
-            onMouseEnter={() => handleLinkMouseEnter(link.id)}
-            onMouseLeave={handleLinkMouseLeave}
-          > 
-          <Link href={link.url} className={styles.link}>
-              {link.title}
-            </Link>
-            {link.submenu && showDropdown === link.id && (
-              <div className={styles.dropdown}>
-                {link.submenu.map((submenuItem) => (
-                  <Link
-                    key={submenuItem.id}
-                    href={submenuItem.url}
-                    className={styles.submenuItem}
-                  >
-                    {submenuItem.title}
-                  </Link>
-                ))}
-              </div>
-            )}
-          </div>
-        ))}
+      <DarkModeToggle />
+        <div
+          onClick={() => setNavActive(!navActive)}
+          className={styles.nav__menu_bar}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`${navActive ? styles.active : ''} ${styles.nav__menu_list}`}>
+          {MENU_LIST.map((menu, idx) => (
+            <div
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
+            >
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          ))}
         {session.status === "authenticated" && (
           <button className={styles.logout} onClick={signOut}>
             Logout
@@ -98,8 +97,9 @@ const Navbar = () => {
             Login
           </button>
         )}
-      </div>
-    </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
