54c59b6729215e70b8e8fc44c2f0523fbeb17cd8
Shawn Lim <e0727020@u.nus.edu>
2023-09-15 15:17:21 +0800

    changed "Job Listings" to "Manage Job Listings" on NavBar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,72 +1,72 @@
-"use client";
-import Link from "next/link";
-import React from "react";
-import styles from "./Navbar.module.css";
-import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
-import { signOut, useSession } from "next-auth/react";
-import { useState } from "react";
-import NavItem from "../navItem/NavItem";
+'use client';
+import Link from 'next/link';
+import React from 'react';
+import styles from './Navbar.module.css';
+import DarkModeToggle from '../DarkModeToggle/DarkModeToggle';
+import { signOut, useSession } from 'next-auth/react';
+import { useState } from 'react';
+import NavItem from '../navItem/NavItem';
 
 const links = [
   {
     id: 1,
-    title: "Home",
-    url: "/",
+    title: 'Home',
+    url: '/',
   },
   {
     id: 2,
-    title: "Portfolio",
-    url: "/portfolio",
+    title: 'Portfolio',
+    url: '/portfolio',
   },
   {
     id: 3,
-    title: "Manage Job Listings",
-    url: "/jobListingManagement",
+    title: 'Manage Job Listings',
+    url: '/jobListingManagement',
   },
   {
     id: 4,
-    title: "Blog",
-    url: "/blog",
+    title: 'Blog',
+    url: '/blog',
   },
   {
     id: 5,
-    title: "Contact",
-    url: "/contact",
+    title: 'Contact',
+    url: '/contact',
   },
   {
     id: 6,
-    title: "Dashboard",
-    url: "/dashboard",
+    title: 'Dashboard',
+    url: '/dashboard',
   },
   {
     id: 7,
-    title: "Chat",
-    url: "/chat",
+    title: 'Chat',
+    url: '/chat',
   },
 ];
 
 const MENU_LIST = [
-  { text: "Home", href: "/" },
-  { text: "Portfolio", href: "/portfolio" },
-  { text: "Job Listings", href: "/jobListings" },
-  { text: "Blog", href: "/blog" },
-  { text: "Contact", href: "/contact" },
-  { text: "Chat", href: "/chat" },
+  { text: 'Home', href: '/' },
+  { text: 'Portfolio', href: '/portfolio' },
+  { text: 'Manage Job Listings', href: '/jobListingManagement' },
+  { text: 'Blog', href: '/blog' },
+  { text: 'Contact', href: '/contact' },
+  { text: 'Chat', href: '/chat' },
 ];
 
 const Navbar = () => {
-  const session = useSession();  
-  
+  const session = useSession();
+
   const [navActive, setNavActive] = useState(null);
   const [activeIdx, setActiveIdx] = useState(-1);
 
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-      <Link href="/" className={styles.logo}>
-        StarHire
-      </Link>
-      <DarkModeToggle />
+        <Link href="/" className={styles.logo}>
+          StarHire
+        </Link>
+        <DarkModeToggle />
         <div
           onClick={() => setNavActive(!navActive)}
           className={styles.nav__menu_bar}
@@ -75,7 +75,11 @@ const Navbar = () => {
           <div></div>
           <div></div>
         </div>
-        <div className={`${navActive ? styles.active : ''} ${styles.nav__menu_list}`}>
+        <div
+          className={`${navActive ? styles.active : ''} ${
+            styles.nav__menu_list
+          }`}
+        >
           {MENU_LIST.map((menu, idx) => (
             <div
               onClick={() => {
@@ -87,16 +91,19 @@ const Navbar = () => {
               <NavItem active={activeIdx === idx} {...menu} />
             </div>
           ))}
-        {session.status === "authenticated" && (
-          <button className={styles.logout} onClick={signOut}>
-            Logout
-          </button>
-        )}
-        {session.status === "unauthenticated" && (
-          <button className={styles.login} onClick={() => window.location.href = "/login"}>
-            Login
-          </button>
-        )}
+          {session.status === 'authenticated' && (
+            <button className={styles.logout} onClick={signOut}>
+              Logout
+            </button>
+          )}
+          {session.status === 'unauthenticated' && (
+            <button
+              className={styles.login}
+              onClick={() => (window.location.href = '/login')}
+            >
+              Login
+            </button>
+          )}
         </div>
       </nav>
     </header>
