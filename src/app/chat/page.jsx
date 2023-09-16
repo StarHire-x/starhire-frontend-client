269a26a7472f2818057d59bfcf6318424ed5df86
Bryan Leong <bryanleongys@gmail.com>
2023-09-16 17:54:55 +0800

    added imports

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -16,6 +16,9 @@ import {
   MessageSeparator,
   Message,
   MessageInput,
+  InfoButton,
+  SendButton,
+  AttachmentButton,
 } from "@chatscope/chat-ui-kit-react";
 import ChatSidebar from "./ChatSidebar";
 import ChatHeader from "./ChatHeader";
