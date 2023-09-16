c46b2a9b967a879a6fc96aeb6f403bb1b90a1430
Shawn Lim <e0727020@u.nus.edu>
2023-09-17 00:06:40 +0800

    moved over UI from jobListingManagementPageMobile to jobListingManagementPage. changed some colours as well. thanks Deshun for helping to improve the UI.

--- src/app/jobListingManagement/page.jsx
+++ src/app/jobListingManagement/page.jsx
@@ -1,19 +1,28 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import JobListingTable from '@/components/JobListingTable/JobListingTable';
-import CreateJobListingForm from '@/components/CreateJobListingForm/CreateJobListingForm';
-import { Toolbar } from 'primereact/toolbar';
 import { Button } from 'primereact/button';
+import { DataView } from 'primereact/dataview';
 import { Dialog } from 'primereact/dialog';
+import { Tag } from 'primereact/tag';
 import { useSession } from 'next-auth/react';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
+import {
+  findAllJobListingsByCorporate,
+  createJobListing,
+  updateJobListing,
+  removeJobListing,
+} from '../api/auth/jobListing/route';
+import styles from './page.module.css';
+import 'primeflex/primeflex.css';
+import CreateJobListingForm from '@/components/CreateJobListingForm/CreateJobListingForm';
 import EditJobListingForm from '@/components/EditJobListingForm/EditJobListingForm';
 
 //this page is viewed by corporate
 const JobListingManagementPage = () => {
-  const [jobListings, setJobListings] = useState([]);
-  const [showCreateDialog, setShowCreateDialog] = useState(false); // State to handle the modal visibility
+  const [jobListing, setJobListing] = useState(null);
+  const [showCreateDialog, setShowCreateDialog] = useState(false);
+  const [showEditDialog, setShowEditDialog] = useState(false);
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const [selectedJobListingData, setSelectedJobListingData] = useState(null);
   const [refreshData, setRefreshData] = useState(false);
   const session = useSession();
 
@@ -30,157 +39,237 @@ const JobListingManagementPage = () => {
   console.log(session);
   console.log(userIdRef);
 
-  /*  
-useEffect(() => {
-    // If session doesn't exist, redirect to login
-    if (!session) {
-      window.location.href = "/login";
-      return; // Exit the useEffect if there's no session
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  const getStatus = (status) => {
+    switch (status) {
+      case 'Verified':
+        return 'success';
+      case 'Unverified':
+        return 'danger';
     }
+  };
 
-    // Function to fetch job listings for the corporate user
-    const fetchCorporateJobListings = async () => {
-      try {
-        const response = await fetch(
-          `http://localhost:8080/job-listing/corporate/${userIdRef}`,
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${session.accessToken}`,
-            },
-          }
-        );
+  const hideEditDialog = () => {
+    setSelectedJobListingData(null);
+    setShowEditDialog(false);
+  };
 
-        // Check for response errors
-        if (!response.ok) {
-          console.error("Error fetching job listings:", await response.text());
-          return;
+  const hideCreateDialog = () => {
+    setShowCreateDialog(false);
+  };
+
+  const hideDeleteDialog = () => {
+    setSelectedJobListingData(null);
+    setShowDeleteDialog(false);
+  };
+
+  const deleteDialogFooter = (
+    <React.Fragment>
+      <Button
+        label="Yes"
+        icon="pi pi-check"
+        onClick={() =>
+          handleDeleteJobListing(selectedJobListingData.jobListingId)
         }
+      />
+    </React.Fragment>
+  );
 
-        const data = await response.json();
-        setJobListings(data);
-      } catch (error) {
-        console.error(
-          "There was an error fetching the job listings:",
-          error.message
-        );
-      }
-    };
-    // Call the function
-    fetchCorporateJobListings();
-  }, []);
-  */
   useEffect(() => {
-    fetch(`http://localhost:8080/job-listing/corporate/${userIdRef}`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      cache: 'no-store',
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setJobListings(data);
-      })
+    findAllJobListingsByCorporate(userIdRef, accessToken)
+      .then((jobListing) => setJobListing(jobListing))
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching user:', error);
       });
-  }, [refreshData, accessToken]);
+  }, [refreshData, userIdRef, accessToken]);
+
+  const itemTemplate = (jobListing) => {
+    return (
+      <div className={styles.card}>
+        <div className={styles.cardHeader}>
+          <h5>{jobListing.title}</h5>
+        </div>
+        <div className={styles.cardBody}>
+          <div className={styles.cardRow}>
+            <span>Job ID:</span>
+            <span>{jobListing.jobListingId}</span>
+          </div>
+          <div className={styles.cardRow}>
+            <span>Location:</span>
+            <span>{jobListing.jobLocation}</span>
+          </div>
+          <div className={styles.cardRow}>
+            <span>Average Salary</span>
+            <span>${jobListing.averageSalary}</span>
+          </div>
+          <div className={styles.cardRow}>
+            <span>Listing Date:</span>
+            <span>{formatDate(jobListing.listingDate)}</span>
+          </div>
+          <div className={styles.cardRow}>
+            <span>Start Date:</span>
+            <span>{formatDate(jobListing.jobStartDate)}</span>
+          </div>
+          <div className={styles.cardRow}>
+            <span>Status:</span>
+            <Tag
+              value={jobListing.jobListingStatus}
+              severity={getStatus(jobListing.jobListingStatus)}
+            />
+          </div>
+        </div>
+        <div className={styles.cardFooter}>
+          <Button
+            label="Edit"
+            icon="pi pi-pencil"
+            className={styles.buttonSpacing}
+            onClick={() => {
+              setSelectedJobListingData(jobListing);
+              setShowEditDialog(jobListing);
+            }}
+          />
+          <Button
+            icon="pi pi-trash"
+            rounded
+            outlined
+            className={styles.buttonSpacing}
+            onClick={() => {
+              setSelectedJobListingData(jobListing);
+              setShowDeleteDialog(jobListing);
+            }}
+          />
+        </div>
+      </div>
+    );
+  };
 
   const handleJobListingCreation = async (newJobListing) => {
     try {
-      // Send the new listing data to the backend
-      const response = await fetch(`http://localhost:8080/job-listing`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify({
-          ...newJobListing,
-          corporateId: userIdRef, // Assuming the session has the user's ID
-        }),
-      });
-
       const payload = {
         ...newJobListing,
         corporateId: userIdRef,
       };
-      console.log('Payload:', payload);
-
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(
-          errorData.message || 'Failed to create the job listing'
-        );
-      }
+      const response = await createJobListing(payload, accessToken);
+      console.log('Created Job listing Successfully', response);
+      alert('Created job listing successfully');
       setRefreshData((prev) => !prev);
-      // Close the modal dialog
-      setShowCreateDialog(false);
     } catch (error) {
       console.error(
         'There was an error creating the job listing:',
         error.message
       );
+      alert('There was an error creating the job listing:');
     }
+    setShowCreateDialog(false);
+  };
+
+  const handleEditJobListing = async (jobListingId, updatedData) => {
+    try {
+      const payload = {
+        ...updatedData,
+        corporateId: userIdRef,
+      };
+      const response = await updateJobListing(
+        payload,
+        jobListingId,
+        accessToken
+      );
+      console.log('Updated Job listing Successfully', response);
+      alert('Updated job listing successfully');
+      setRefreshData((prev) => !prev);
+    } catch (error) {
+      console.error(
+        'There was an error updating the job listing:',
+        error.message
+      );
+      alert('There was an error updating the job listing:');
+    }
+    setSelectedJobListingData(null);
+    setShowEditDialog(false);
+  };
+
+  const handleDeleteJobListing = async (jobListingId) => {
+    try {
+      const response = await removeJobListing(jobListingId, accessToken);
+      console.log('User is deleted', response);
+      alert('Deleted job listing successfully');
+      setRefreshData((prev) => !prev);
+    } catch (error) {
+      console.error('Error deleting user:', error);
+      alert('There was an error deleting the job listing:');
+    }
+    setSelectedJobListingData(null);
+    setShowDeleteDialog(false);
   };
 
   const header = (
     <div className="p-d-flex p-jc-between">
-      <h2 style={styles.title}>Job Listing Management</h2>
-      <div className="p-d-flex p-ai-end">
-        <Button
-          label="Add A Job Listing"
-          onClick={() => setShowCreateDialog(true)}
-        />
-      </div>
+      <h2 className={styles.headerTitle}>Job Listing Management</h2>
+      <Button
+        label="Add A Job Listing"
+        onClick={() => setShowCreateDialog(true)}
+      />
     </div>
   );
 
   return (
-    <div style={styles.container}>
-      <Toolbar left={header}></Toolbar>
-      <div style={styles.tableContainer}>
-        <JobListingTable listings={jobListings} />
-      </div>
+    <div className="container">
+      <DataView
+        value={jobListing}
+        layout="grid"
+        rows={10}
+        paginator
+        header={header}
+        currentPageReportTemplate="Showing {first} to {last} of {totalRecords} entries"
+        rowsPerPageOptions={[10, 25, 50]}
+        emptyMessage="No job listing found"
+        itemTemplate={itemTemplate}
+        pt={{
+          grid: { className: 'surface-ground' },
+        }}
+      />
+
       <Dialog
         header="Create Job Listing"
         visible={showCreateDialog}
-        onHide={() => setShowCreateDialog(false)}
-        style={{ width: '50vw' }}
+        onHide={hideCreateDialog}
+        className={styles.cardDialog}
       >
         <CreateJobListingForm onCreate={handleJobListingCreation} />
       </Dialog>
+
+      <Dialog
+        header="Edit Job Listing"
+        visible={showEditDialog}
+        onHide={hideEditDialog}
+        className={styles.cardDialog}
+      >
+        <EditJobListingForm
+          initialData={showEditDialog}
+          onSave={(updatedData) => {
+            handleEditJobListing(showEditDialog.jobListingId, updatedData);
+          }}
+        />
+      </Dialog>
+
+      <Dialog
+        header="Delete Job Listing"
+        visible={showDeleteDialog}
+        onHide={hideDeleteDialog}
+        className={styles.cardDialog}
+        footer={deleteDialogFooter}
+      >
+        <h3>
+          Confirm Delete Job ID:{' '}
+          {showDeleteDialog && showDeleteDialog.jobListingId}?
+        </h3>
+      </Dialog>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    backgroundColor: '#edf6f9',
-    padding: '40px 60px',
-    borderRadius: '10px',
-    boxShadow: '0 5px 15px rgba(0, 0, 0, 0.1)',
-    margin: '40px auto',
-    maxWidth: '1200px',
-  },
-  title: {
-    color: '#006d77',
-    fontSize: '32px',
-    fontWeight: 'bold',
-    marginBottom: '20px',
-    borderBottom: '3px solid #e29578',
-    paddingBottom: '10px',
-  },
-  tableContainer: {
-    width: '100%',
-  },
 };
 
 export default JobListingManagementPage;
