a147ed959f120f91cbd75f6aaf5ab798921defbb
Alwin Ng Jun Wei <alwinngjw@gmail.com>
2023-09-27 12:12:07 +0800

    added page to view job seeker, still work in progress

--- src/app/jobListingManagement/viewAllMyJobListings/viewJobSeeker/page.jsx
+++ src/app/jobListingManagement/viewAllMyJobListings/viewJobSeeker/page.jsx
@@ -4,18 +4,21 @@ import { DataView } from 'primereact/dataview';
 import { Dialog } from 'primereact/dialog';
 import { Tag } from 'primereact/tag';
 import { useSession } from 'next-auth/react';
-
-
-import { getJobApplicationsByJobListingId } from '@/app/api/auth/jobListing/route';
+import { Button } from 'primereact/button';
+import { getJobSeekersByJobApplicationId } from '@/app/api/auth/jobListing/route';
 import styles from 'src/app/jobListingManagement/page.module.css';
 import 'primeflex/primeflex.css';
 import { useRouter } from 'next/navigation';
 import { useSearchParams } from 'next/navigation';
-//import "./styles.css";
-const ViewApplicationsPage = () => {
-    const [jobApplications, setJobApplications] = useState(null);
+import { Card } from 'primereact/card';
+import Image from 'next/image';
+import "./styles.css";
+
+const ViewJobSeekerPage = () => {
+    const [jobApplication, setJobApplications] = useState(null);
     const [refreshData, setRefreshData] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
+    const [userDialog, setUserDialog] = useState(false);
     const session = useSession();
     const router = useRouter();
   
@@ -39,10 +42,11 @@ const ViewApplicationsPage = () => {
 
     useEffect(() => {
       if (accessToken) {
-        getJobApplicationsByJobListingId(id, accessToken)
+        getJobSeekersByJobApplicationId(id, accessToken)
           .then((response) => {
             setJobApplications(response);
             setIsLoading(false);
+            console.log(jobApplication);
           })
           .catch((error) => {
             console.error("Error fetching job listings:", error);
@@ -51,80 +55,131 @@ const ViewApplicationsPage = () => {
       }
     }, [userIdRef, accessToken]);
 
-
-    const itemTemplate = (jobApplications) => {
-      const cardLink = `/jobListingManagement/viewAllMyJobListings/viewJobSeeker?id=${jobApplications.jobApplicationId}`;
-      return (
-        <a href={cardLink} className={styles.cardLink}>
-          <div className={styles.card}>
-            <div className={styles.cardHeader}>
-              <h3>{"Application ID: " + jobApplications.jobApplicationId}</h3>
-            </div>
-            <div className={styles.cardBody}>
-              {isLoading ? (
-                <div className="loading-animation">
-                  <div className="spinner"></div>
-                </div>
-              ) : (
-                <>
-                  <div className={styles.cardRow}>
-                    <span>Available start Date:</span>
-                    <span>
-                      {formatDate(jobApplications.availableStartDate)}
-                    </span>
-                  </div>
-                  <div className={styles.cardRow}>
-                    <span>Available End Date:</span>
-                    <span>{formatDate(jobApplications.availableEndDate)}</span>
-                  </div>
-                  <div className={styles.cardRow}>
-                    <span>Submitted on:</span>
-                    <span>{formatDate(jobApplications.submissionDate)}</span>
-                  </div>
-
-                  <div className={styles.cardRow}>
-                    <span>Status:</span>
-                    <span
-                      className={
-                        jobApplications.jobApplicationStatus === "Accepted"
-                          ? styles.greenText
-                          : styles.redText
-                      }
-                    >
-                      {jobApplications.jobApplicationStatus}
-                    </span>
-                  </div>
-                </>
-              )}
-            </div>
-          </div>
-        </a>
-      );
+    const hideDialog = () => {
+      setUserDialog(false);
     };
-    
-    if (session.status === 'authenticated') {
-      return (
-        <div className={styles.container}>
-          <DataView
-            value={jobApplications}
-            className={styles.dataViewContainer}
-            layout="grid"
-            rows={10}
-            paginator
-            header={<h2 className={styles.headerTitle}>All Applications for Current Job Listing</h2>}
-            currentPageReportTemplate="Showing {first} to {last} of {totalRecords} entries"
-            rowsPerPageOptions={[10, 25, 50]}
-            emptyMessage="No Application found for this Job Listing"
-            itemTemplate={itemTemplate}
-            pt={{
-              grid: { className: 'surface-ground' },
-            }}
-          />
-        </div>
-      );
-    }
+  
+    const showUserDialog = (action) => {
+      setUserDialog(true);
+      setStatus(action);
+    };
+  
+    const userDialogFooter = (
+      <React.Fragment>
+        <Button label="Cancel" icon="pi pi-times" outlined onClick={hideDialog} />
+        <Button
+          label="Yes"
+          icon="pi pi-check"
+          outlined
+          //onClick={() => updateJobListingStatus(status)}
+        />
+      </React.Fragment>
+    );
+  
+    const footer = (
+      <div className="flex flex-wrap justify-content-end gap-2">
+        <Button
+          label="Approve"
+          icon="pi pi-check"
+          className="approve-button p-button-outlined p-button-secondary"
+          onClick={() => showUserDialog('Active')}
+        />
+        <Button
+          label="Reject"
+          icon="pi pi-times"
+          className="reject-button p-button-outlined p-button-secondary"
+          onClick={() => showUserDialog('Inactive')}
+        />
+        <Button
+          label="Archive"
+          icon="pi pi-folder"
+          className="archive-button p-button-outlined p-button-secondary"
+          onClick={() => showUserDialog('Inactive')}
+        />
+      </div>
+    );
+  
+    return (
+      <div className="container">
+        {isLoading ? (
+          <div className="loading-animation">
+            <div className="spinner"></div>
+          </div>
+        ) : (
+          <div>
+            <Card
+              title={"Job Application: " + jobApplication.jobApplicationId}
+              //subTitle={jobApplication.jobSeeker.userName}
+              footer={footer}
+              className="my-card"
+              style={{ borderRadius: '0' }}
+            >
+              <div className="my-card.p-card-content">
+                <div className="company-info">
+                {jobApplication.jobApplicationId === "" ? (
+                    <Image src={HumanIcon} alt="User" className="avatar" />
+                  ) : (
+                  <img
+                    src={jobApplication.jobSeeker.profilePictureUrl}
+                    className="avatar"
+                  />
+                )}
+                  
+                  <div className="company-details">
+                    <p>{jobApplication.jobApplicationId}</p>
+                  </div>
+                </div>
+  
+                <strong>Username: </strong>
+                <p>{jobApplication.jobSeeker.userName}</p>
+                <strong>Job Responsibilities</strong>
+                <p>{jobApplication.jobApplicationId}</p>
+                <strong>Job Requirements</strong>
+                <p>{jobApplication.jobApplicationId}</p>
+                <strong>Average Salary</strong>
+                <p>{"$" + jobApplication.jobApplicationId + " SGD"}</p>
+                <strong>Job Start Date</strong>
+  
+                <div className="contact-info">
+                  <strong>Contact Information</strong>
+                  <p>{jobApplication.jobApplicationId}</p>
+                  <p className="second-p">{jobApplication.jobApplicationId}</p>
+                </div>
+  
+                <strong>Job Seeker Details</strong>
+                <p>{"Email: " + jobApplication.jobSeeker.email}</p>
+                <p className="second-p">{"Address: " + jobApplication.jobApplicationId}</p>
+  
+  
+                <p>{"Job Listing ID: " + jobApplication.jobApplicationId}</p>
+  
+                <strong>Current Status of Job</strong>
+                <p
+                  style={{
+                    color:
+                    jobApplication.jobApplicationIds === 'Active' ? 'green' : 'red',
+                  }}
+                >
+                  {jobApplication.jobApplicationId}
+                </p>
+              </div>
+            </Card>
+  
+            <Dialog
+              visible={userDialog}
+              style={{ width: '32rem' }}
+              breakpoints={{ '960px': '75vw', '641px': '90vw' }}
+              header="Confirm?"
+              className="p-fluid"
+              footer={userDialogFooter}
+              onHide={hideDialog}
+            ></Dialog>
+          </div>
+        )}
+      </div>
+    );
 
     
 }
 
-export default ViewApplicationsPage;
+export default ViewJobSeekerPage;
