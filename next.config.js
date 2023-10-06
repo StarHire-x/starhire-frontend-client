546e00898286ca9bc71501ae9ffb1a0127045958
Lukas Lee <jiawei3e1@gmail.com>
2023-10-06 15:40:23 +0800

    changes requested by deshun during dry run

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // env: {
-  //   NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-  // },
-};
-
-module.exports = {
   images: {
-    domains: ['starhire-uploader.s3.ap-southeast-2.amazonaws.com'],
+    domains: ["starhire-uploader.s3.ap-southeast-2.amazonaws.com"],
   },
 };
+
+module.exports = nextConfig
