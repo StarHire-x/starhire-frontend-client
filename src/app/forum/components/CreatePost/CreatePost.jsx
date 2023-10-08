ac9750566b2a091fb48d952a984f0a3af5ac84b6
hengyjj <hengyjj@hotmail.com>
2023-10-08 10:37:53 +0800

    set up modal for create post and fixed the styling max-width for modal

---
+++ src/app/forum/components/CreatePost/CreatePost.jsx
@@ -0,0 +1,13 @@
+"use client";
+import { useState, useEffect } from "react";
+import styles from "./CreatePost.module.css";
+
+const CreatePost = () => {
+  return (
+    <>
+      <h2>Create post here </h2>
+    </>
+  );
+};
+
+export default CreatePost;
--- src/app/forum/components/ForumCreatePostButton/ForumCreatePostButton.jsx
+++ src/app/forum/components/ForumCreatePostButton/ForumCreatePostButton.jsx
@@ -1,12 +1,35 @@
 "use client";
 
+import { useState } from "react";
 import { Button } from "primereact/button";
-import styles from './ForumCreatePostButton.module.css';
+import styles from "./ForumCreatePostButton.module.css";
+import CreatePost from "../CreatePost/CreatePost";
+import { Dialog } from "primereact/dialog";
 
 const ForumCreatePostButton = () => {
+  const[visible, setVisible] = useState(false);
+
+  const handleOnClick = () => {
+    setVisible(true);
+  }
+
+  const onHide = () => {
+    setVisible(false);
+  }
+
   return (
     <>
-      <Button size="small" rounded className={styles.createPostBtn} label="Create Post" icon="pi pi-plus" />
+      <Button
+        size="small"
+        rounded
+        className={styles.createPostBtn}
+        label="Create Post"
+        icon="pi pi-plus"
+        onClick={handleOnClick}
+      />
+      <Dialog header="Create a Post" visible={visible} onHide={onHide} className={styles.createPostDialog}>
+        <CreatePost />
+      </Dialog>
     </>
   );
 };
