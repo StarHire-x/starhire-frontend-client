6610cfe035092b49a39edb9185dc18e5c27456a3
Lukas Lee <jiawei3e1@gmail.com>
2023-10-09 18:28:13 +0800

    fixed annoying error popup about access token and refreshing the forum page won't log you out now

--- src/app/forum/page.jsx
+++ src/app/forum/page.jsx
@@ -30,7 +30,7 @@ const ForumPage = () => {
 
   if (
     session.status === "unauthenticated" ||
-    currentUserRole !== Enums.JOBSEEKER
+    (session.status === "authenticated" && currentUserRole !== Enums.JOBSEEKER)
   ) {
     router.push("/login");
   }
@@ -57,17 +57,19 @@ const ForumPage = () => {
   }, []);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getAllForumCategories(accessToken);
-        const myPostMenu = { forumCategoryTitle: "My Posts" };
-        response.unshift(myPostMenu); // show 'My Posts' menu as first option
-        setForumCategories(response);
-      } catch (error) {
-        console.error("Error fetching forum categories:", error);
-      }
-    };
-    fetchData();
+    if (accessToken) {
+      const fetchData = async () => {
+        try {
+          const response = await getAllForumCategories(accessToken);
+          const myPostMenu = { forumCategoryTitle: "My Posts" };
+          response.unshift(myPostMenu); // show 'My Posts' menu as first option
+          setForumCategories(response);
+        } catch (error) {
+          console.error("Error fetching forum categories:", error);
+        }
+      };
+      fetchData();
+    }
   }, [accessToken]);
 
   const forumCategoryTitleToId = {};
@@ -80,7 +82,8 @@ const ForumPage = () => {
     const fetchData = async () => {
       if (
         forumCategoryTitle !== "My Posts" &&
-        forumCategoryTitle !== "Recent Posts"
+        forumCategoryTitle !== "Recent Posts" &&
+        accessToken
       ) {
         //don't fetch "My Posts" & "Recents Posts" here. It will be a different API method. Don't remove this line else will have console log error.
         const forumCategoryId = forumCategoryTitleToId[forumCategoryTitle];
@@ -96,7 +99,7 @@ const ForumPage = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      if (forumCategoryTitle === "My Posts") {
+      if (forumCategoryTitle === "My Posts" && accessToken) {
         const response = await getAllForumPostsByJobSeeker(
           userIdRef,
           accessToken
@@ -109,7 +112,7 @@ const ForumPage = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      if (forumCategoryTitle === "Recent Posts") {
+      if (forumCategoryTitle === "Recent Posts" && accessToken) {
         const response = await getAllSortedForumPosts(accessToken);
         setForumPosts(response);
       }
