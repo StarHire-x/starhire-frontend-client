e360ed9a1711a278be2781f4bf4b996f86667751
hengyjj <hengyjj@hotmail.com>
2023-10-09 18:47:16 +0800

    edited styling

--- src/app/forum/components/ForumPosts/ForumPosts.jsx
+++ src/app/forum/components/ForumPosts/ForumPosts.jsx
@@ -8,29 +8,40 @@ import moment from "moment";
 const ForumPosts = ({ forumPosts }) => {
   const formatRawDate = (rawDate) => {
     return moment(rawDate).format("DD MMMM YYYY, hh:mm A");
-  }
+  };
 
   const onCommentClick = () => {
     //leave a comment here
-  }
-    
+  };
+
   const itemTemplate = (data) => {
+    const categoryClass = `categoryTag-${data.forumCategory.forumCategoryTitle}`;
+    console.log(categoryClass);
+
     return (
       <div className={styles.postContainer}>
         <div className={styles.postTitle}>{data.forumPostTitle}</div>
 
         <div className={styles.postInfo}>
-          <div className={styles.idTag}>{data.forumPostId}</div>
-          <div className={styles.categoryTag}>{data.forumCategory.forumCategoryTitle}</div>
-          <div className={styles.dateTimeText}>{formatRawDate(data.createdAt)}</div>
+          <div className={styles.idTag}>#{data.forumPostId}</div>
+          <div className={styles.categoryTag}>
+            {data.forumCategory.forumCategoryTitle}
+          </div>
         </div>
 
-        <div className={styles.content}>
-        {data.forumPostMessage}
-        </div>
+        <div className={styles.content}>{data.forumPostMessage}</div>
 
         <div className={styles.footer}>
-            <Button size="small" icon="pi pi-comments" rounded onClick={onCommentClick}></Button>
+          <div className={styles.dateTimeText}>
+            {formatRawDate(data.createdAt)}
+          </div>
+          <Button
+            size="small"
+            icon="pi pi-comments"
+            rounded
+            onClick={onCommentClick}
+            className={styles.commentButton}
+          ></Button>
         </div>
       </div>
     );
