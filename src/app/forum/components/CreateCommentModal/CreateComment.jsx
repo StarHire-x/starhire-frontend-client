57edf53fd072bcbf9fdfaf31e5ff320fa666961b
hengyjj <hengyjj@hotmail.com>
2023-10-10 12:09:25 +0800

    fixed the styling for create-comment-modal

--- src/app/forum/components/CreateCommentModal/CreateComment.jsx
+++ src/app/forum/components/CreateCommentModal/CreateComment.jsx
@@ -6,8 +6,38 @@ import { Card } from "primereact/card";
 import moment from "moment";
 
 const CreateComment = ({ userIdRef, accessToken, postData }) => {
+  const [comment, setComment] = useState("");
+  const maxCharacterCount = 8000;
+
   const formatRawDate = (rawDate) => {
     return moment(rawDate).format("DD MMMM YYYY, hh:mm A");
+  };
+
+  const [formData, setFormData] = useState({
+    createdAt: new Date(),
+    isAnonymous: false,
+    // forumPostId: "",
+    jobSeekerId: userIdRef,
+    forumCommentMessage: "",
+  });
+
+  // const handleCommentChange = (e) => {
+  //   setComment(e.target.value);
+  //   setFormData((prevData) => ({
+  //     ...prevData,
+  //     forumCommentMessage: e.target.value,
+  //   }));
+  // };
+
+  const handleCommentChange = (e) => {
+    const inputValue = e.target.value;
+    if (inputValue.length <= maxCharacterCount) {
+      setComment(inputValue);
+      setFormData((prevData) => ({
+        ...prevData,
+        forumCommentMessage: e.target.value,
+      }));
+    }
   };
 
   return (
@@ -49,6 +79,21 @@ const CreateComment = ({ userIdRef, accessToken, postData }) => {
       <div className={styles.leaveCommentContainer}>
         <div className={styles.leaveCommentHeader}>
           <h3>Leave a comment!</h3>
+        </div>
+        <div className={styles.leaveCommentContent}>
+          <InputTextarea
+            rows={2}
+            cols={75}
+            value={comment}
+            onChange={(e) => handleCommentChange(e)}
+            className={styles.textarea}
+          />
+        </div>
+        <div className={styles.characterCount}>{maxCharacterCount - comment.length} characters left</div>
+      </div>
+      <div className={styles.allCommentsContainer}>
+        <div className={styles.allCommentsHeader}>
+          <h3>All Comments</h3>
         </div>
       </div>
     </>
