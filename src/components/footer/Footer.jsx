be0ecc06a0edd5666d0f6154e53feb0deb6d64b3
rilwan99 <rilwanwild@gmail.com>
2023-10-11 22:15:29 +0800

    edit footer styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,38 +2,15 @@
 
 import React from "react";
 import styles from "./footer.module.css";
-import Image from "next/image";
-import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 const Footer = () => {
-  /*
-  const session = useSession();
-  let roleRef, sessionTokenRef, userIdRef;
-
-  if (session && session.data && session.data.user) {
-    userIdRef = session.data.user.userId;
-    roleRef = session.data.user.role;
-    sessionTokenRef = session.data.user.accessToken;
-  }
-
   return (
     <div className={styles.container}>
+      <div>
       <p>©2023 StarHire. All rights reserved</p>
-      {session?.status === "authenticated" ? (
-        <p className={styles.reportIssue}>
-          <a href="ticketManagement">Report an Issue</a>
-        </p>
-      ) : null}
-    </div>
-  );
-};
-*/
-  return (
-    <div className={styles.container}>
-      <p>©2023 StarHire. All rights reserved</p>
-        <p className={styles.reportIssue}>
-          <a href="/ticketManagement">Help Center</a>
-        </p>
+      </div>
+      <Link className={styles.reportIssue} href="/ticketManagement">Need Help?</Link>
     </div>
   );
 };
