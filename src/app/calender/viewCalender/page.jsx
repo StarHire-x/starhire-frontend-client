d9303bf0aca02ee65abf9a4f7affb5a35c175737
Alwin Ng Jun Wei <alwinngjw@gmail.com>
2023-10-15 14:28:14 +0800

    added new look for calender

--- src/app/calender/viewCalender/page.jsx
+++ src/app/calender/viewCalender/page.jsx
@@ -1,95 +1,102 @@
 "use client"
 
-import Layout from './components/layout';
+import React, { useState, useRef, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
 import timeGridPlugin from '@fullcalendar/timegrid';
-
-/*
-export default function CalendarPage() {
-  const events = [
-    {
-      title: 'Event 1',
-      start: '2023-10-25T10:00:00',
-      end: '2023-10-25T12:00:00',
-      resourceId: 'a', // Resource ID as defined in the resources array
-    },
-    {
-      title: 'Event 2',
-      start: '2023-10-26T14:00:00',
-      end: '2023-10-26T16:00:00',
-      resourceId: 'b', // Resource ID as defined in the resources array
-    },
-  ];
-
-  return (
-    <Layout>
-      <div className='calendar-container'>
-        <FullCalendar
-          plugins={[
-            resourceTimelinePlugin,
-            dayGridPlugin,
-            interactionPlugin,
-            timeGridPlugin,
-          ]}
-          headerToolbar={{
-            left: 'prev,next today',
-            center: 'title',
-            right: 'resourceTimelineWeek,dayGridMonth,timeGridWeek',
-          }}
-          initialView='resourceTimelineWeek'
-          nowIndicator={true}
-          editable={true}
-          selectable={true}
-          selectMirror={true}
-          schedulerLicenseKey='CC-Attribution-NonCommercial-NoDerivatives' //May have to review this license when we go commercial (But for dev reasons, this shud do)
-          resources={[
-            { id: 'a', title: 'Auditorium A' },
-            { id: 'b', title: 'Auditorium B', eventColor: 'green' },
-            { id: 'c', title: 'Auditorium C', eventColor: 'orange' },
-          ]}
-          events={events}
-        />
-      </div>
-    </Layout>
-  );
-}
-*/
-
-import React, { useState } from 'react'; // Import useState
+import Layout from './components/layout';
+import { Button } from 'primereact/button';
+import { Calendar } from 'primereact/calendar';
+import { Dialog } from 'primereact/dialog';
+import { InputText } from 'primereact/inputtext';
 
 export default function CalendarPage() {
-  const [isFormVisible, setIsFormVisible] = useState(false); // State for modal visibility
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [isConfirmationVisible, setIsConfirmationVisible] = useState(false); // Add confirmation dialog state
+  const [eventClicked, setEventClicked] = useState(false); 
+  const [warningDialogVisible, setWarningDialogVisible] = useState(false);
+  const [selectedMeetingLink, setSelectedMeetingLink] = useState('');
+
   const [events, setEvents] = useState([
     {
       title: 'Event 1',
       start: '2023-10-25T10:00:00',
       end: '2023-10-25T12:00:00',
       resourceId: 'a',
+      onlineMeetingLink: 'https://example.com/meeting1',
     },
     {
       title: 'Event 2',
       start: '2023-10-26T14:00:00',
       end: '2023-10-26T16:00:00',
       resourceId: 'b',
+      onlineMeetingLink: 'https://example.com/meeting2',
     },
   ]);
 
-  // Function to add a new event
+  const calendarRef = useRef(null);
+
+  const [eventData, setEventData] = useState({
+    title: '',
+    start: null,
+    end: null,
+    resourceId: 'a',
+    onlineMeetingLink: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsConfirmationVisible(true); // Show confirmation dialog
+  };
+
   const addNewEvent = (eventData) => {
     setEvents([...events, eventData]);
-    setIsFormVisible(false); // Close the form after adding an event
+    setIsFormVisible(false);
+  };
+
+  const handleConfirmation = (confirmed) => {
+    setIsConfirmationVisible(false); // Close the confirmation dialog
+    if (confirmed) {
+      addNewEvent(eventData); // Add the event when confirmed
+      setIsFormVisible(false); // Close the event creation dialog
+    }
+  };
+
+  useEffect(() => {
+    if (calendarRef.current) {
+      const calendarApi = calendarRef.current.getApi();
+      calendarApi.on('eventClick', handleEventClick);
+    }
+  }, []);
+
+  const handleEventClick = (eventInfo) => {
+    if (!eventClicked) {
+      setWarningDialogVisible(true);
+      setSelectedMeetingLink(eventInfo.event.extendedProps.onlineMeetingLink);
+    }
+  };
+
+  const closeWarningDialog = () => {
+    setWarningDialogVisible(false); // Close the warning dialog
+  };
+
+  const redirectToMeetingLink = () => {
+    if (selectedMeetingLink) {
+      window.open(selectedMeetingLink, "_blank");
+      setEventClicked(true);
+      setWarningDialogVisible(false);
+    }
   };
 
   return (
     <Layout>
-      <div className='calendar-container'>
-        {/* Button to open the form */}
-        <button onClick={() => setIsFormVisible(true)}>Add Event</button>
+      <div className="calendar-container">
+        <Button label="Add Event" onClick={() => setIsFormVisible(true)} />
 
         <FullCalendar
+          ref={calendarRef}
           plugins={[
             resourceTimelinePlugin,
             dayGridPlugin,
@@ -101,12 +108,12 @@ export default function CalendarPage() {
             center: 'title',
             right: 'resourceTimelineWeek,dayGridMonth,timeGridWeek',
           }}
-          initialView='resourceTimelineWeek'
+          initialView="resourceTimelineWeek"
           nowIndicator={true}
           editable={true}
           selectable={true}
           selectMirror={true}
-          schedulerLicenseKey='CC-Attribution-NonCommercial-NoDerivatives'
+          schedulerLicenseKey="CC-Attribution-NonCommercial-NoDerivatives"
           resources={[
             { id: 'a', title: 'Auditorium A' },
             { id: 'b', title: 'Auditorium B', eventColor: 'green' },
@@ -116,60 +123,109 @@ export default function CalendarPage() {
         />
       </div>
 
-      {isFormVisible && (
-        <EventForm addNewEvent={addNewEvent} onClose={() => setIsFormVisible(false)} />
-      )}
+      <Dialog
+        header="Schedule a time"
+        visible={isFormVisible}
+        style={{ width: '50vw' }}
+        onHide={() => setIsFormVisible(false)}
+        footer={
+          <div>
+            <Button label="Add Event" type="submit" onClick={handleSubmit} />
+            <Button label="Cancel" className="p-button-secondary" onClick={() => setIsFormVisible(false)} />
+          </div>
+        }
+      >
+        <form onSubmit={handleSubmit}>
+          <div className="p-field">
+            <label htmlFor="title" className="p-sr-only">
+              Title:
+            </label>
+            <InputText
+              id="title"
+              type="text"
+              value={eventData.title}
+              onChange={(e) => setEventData({ ...eventData, title: e.target.value })}
+              placeholder="Title"
+            />
+          </div>
+          <div className="p-field">
+            <label htmlFor="start" className="p-sr-only">
+              Start Date and Time:
+            </label>
+            <Calendar
+              id="start"
+              value={eventData.start}
+              onChange={(e) => setEventData({ ...eventData, start: e.value })}
+              showTime
+              placeholder="Start Date and Time"
+            />
+          </div>
+          <div className="p-field">
+            <label htmlFor="end" className="p-sr-only">
+              End Date and Time:
+            </label>
+            <Calendar
+              id="end"
+              value={eventData.end}
+              onChange={(e) => setEventData({ ...eventData, end: e.value })}
+              showTime
+              placeholder="End Date and Time"
+            />
+          </div>
+          <div className="p-field">
+            <label htmlFor="onlineMeetingLink" className="p-sr-only">
+              Online Meeting Link:
+            </label>
+            <InputText
+              id="onlineMeetingLink"
+              type="url"
+              value={eventData.onlineMeetingLink}
+              onChange={(e) => setEventData({ ...eventData, onlineMeetingLink: e.target.value })}
+              placeholder="Online Meeting Link"
+            />
+          </div>
+        </form>
+      </Dialog>
+
+      <Dialog
+        header="Confirmation"
+        visible={isConfirmationVisible}
+        style={{ width: '30vw' }}
+        onHide={() => setIsConfirmationVisible(false)}
+        footer={
+          <div>
+            <Button label="Cancel" className="p-button-secondary" onClick={() => handleConfirmation(false)} />
+            <Button label="Confirm" type="button" onClick={() => handleConfirmation(true)} />
+          </div>
+        }
+      >
+        <p>Add this Interview Date?</p>
+      </Dialog>
+
+      <Dialog
+        header="Warning"
+        visible={warningDialogVisible}
+        style={{ width: '30vw' }}
+        onHide={closeWarningDialog}
+        footer={
+          <div>
+            <Button label="OK" onClick={redirectToMeetingLink} />
+          </div>
+        }
+      >
+        <p>Warning! You will be redirected to a new link.</p>
+      </Dialog>
     </Layout>
   );
 }
 
-function EventForm({ addNewEvent, onClose }) {
-  const [eventData, setEventData] = useState({
-    title: '',
-    start: '',
-    end: '',
-    resourceId: 'a', 
-  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    addNewEvent(eventData);
-  };
 
-  return (
-    <div className='event-form'>
-      <h2>Create New Event</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Title:
-          <input
-            type='text'
-            value={eventData.title}
-            onChange={(e) => setEventData({ ...eventData, title: e.target.value })}
-          />
-        </label>
-        <label>
-          Start Date and Time:
-          <input
-            type='datetime-local'
-            value={eventData.start}
-            onChange={(e) => setEventData({ ...eventData, start: e.target.value })}
-          />
-        </label>
-        <label>
-          End Date and Time:
-          <input
-            type='datetime-local'
-            value={eventData.end}
-            onChange={(e) => setEventData({ ...eventData, end: e.target.value })}
-          />
-        </label>
-        <button type='submit'>Add Event</button>
-        <button type='button' onClick={onClose}>
-          Cancel
-        </button>
-      </form>
-    </div>
-  );
-}
+
+
+
+
+
+
+
 
