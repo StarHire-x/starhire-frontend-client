1b6623cdf17430ce237057828509560628afe190
rilwan99 <rilwanwild@gmail.com>
2023-10-18 15:48:57 +0800

    update missing import

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
