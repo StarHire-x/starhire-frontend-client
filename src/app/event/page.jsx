c11bcd5717f03b3a293eef8aabd4bcfb2f07a0e1
Shawn Lim <e0727020@u.nus.edu>
2023-10-24 21:48:11 +0800

    view all events on the platform done for job seekers

--- src/app/event/page.jsx
+++ src/app/event/page.jsx
@@ -9,6 +9,7 @@ import { Dialog } from 'primereact/dialog';
 import { InputText } from 'primereact/inputtext';
 import { ProgressSpinner } from 'primereact/progressspinner';
 import { Toast } from 'primereact/toast';
+import { findAllEventListings } from '../api/eventListing/route';
 import styles from './page.module.css';
 
 const EventPage = () => {
@@ -18,7 +19,6 @@ const EventPage = () => {
   const [eventListings, setEventListings] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [refreshData, setRefreshData] = useState(false);
-  const [selectedEvent, setSelectedEvent] = useState(null);
 
   const accessToken =
     session.status === 'authenticated' &&
@@ -29,8 +29,105 @@ const EventPage = () => {
     session.status === 'authenticated' &&
     session.data &&
     session.data.user.userId;
-};
 
-const toast = useRef(null);
+  const toast = useRef(null);
+
+  useEffect(() => {
+    if (session.status === 'unauthenticated' || session.status === 'loading') {
+      router.push('/login');
+    }
+
+    if (accessToken) {
+      findAllEventListings(accessToken)
+        .then((data) => {
+          setEventListings(data);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.error('Error fetching event listings:', error);
+          setIsLoading(false);
+        });
+    }
+  }, [refreshData, accessToken]);
+
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  const eventTemplate = (eventData) => {
+    return (
+      <div className={styles.event}>
+        <img
+          src={eventData.image}
+          alt={eventData.eventName}
+          className={styles.eventImage}
+        />
+        <h4>{eventData.eventName}</h4>
+        <p>
+          <strong>Location:</strong> {eventData.location}
+        </p>
+        <p>
+          <strong>Date:</strong> {formatDate(eventData.eventDate)}
+        </p>
+        <p>
+          <strong>Details:</strong> {eventData.details}
+        </p>
+        <p>
+          <strong>Listing Date:</strong> {formatDate(eventData.listingDate)}
+        </p>
+        <p>
+          <strong>Status:</strong> {eventData.eventListingStatus}
+        </p>
+        <p>
+          <strong>Organized By:</strong>{' '}
+          {eventData.corporate && eventData.corporate.userName}
+        </p>
+      </div>
+    );
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.header}>Upcoming Events For You</h1>
+      <Toast ref={toast} />
+      {isLoading ? (
+        <ProgressSpinner
+          style={{
+            display: 'flex',
+            height: '100vh',
+            'justify-content': 'center',
+            'align-items': 'center',
+          }}
+        />
+      ) : (
+        <Carousel
+          value={eventListings}
+          itemTemplate={eventTemplate}
+          numVisible={3}
+          numScroll={1}
+          responsiveOptions={[
+            {
+              breakpoint: '1024px',
+              numVisible: 3,
+              numScroll: 3,
+            },
+            {
+              breakpoint: '600px',
+              numVisible: 2,
+              numScroll: 2,
+            },
+            {
+              breakpoint: '480px',
+              numVisible: 1,
+              numScroll: 1,
+            },
+          ]}
+          circular={true}
+        />
+      )}
+    </div>
+  );
+};
 
 export default EventPage;
