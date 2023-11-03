9c851d853949dc8d0b4f889ea2131cacb266c294
Alwin Ng Jun Wei <alwinngjw@gmail.com>
2023-11-03 11:25:28 +0800

    added viewAllPremiumUsers method

--- src/app/api/eventListing/route.js
+++ src/app/api/eventListing/route.js
@@ -128,3 +128,26 @@ export const removeEventListing = async (id, accessToken) => {
     throw error;
   }
 };
+
+export const viewAllPremiumUsers = async (accessToken) => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/corporate/premium-users`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      const errorData = await res.json();
+      console.log(errorData);
+      throw new Error(errorData.message);
+    }
+    return await res.json();
+  } catch (error) {
+    console.log("There was a problem fetching all Promotion Request", error);
+    throw error;
+  }
+};
