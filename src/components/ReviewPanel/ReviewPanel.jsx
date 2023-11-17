966bafeb6b60166466093f72e9406af6781491ea
Shawn Lim <e0727020@u.nus.edu>
2023-11-17 12:28:33 +0800

    removed unused imports

--- src/components/ReviewPanel/ReviewPanel.jsx
+++ src/components/ReviewPanel/ReviewPanel.jsx
@@ -1,18 +1,20 @@
-import React, { useState, useRef } from "react";
-import { Panel } from "primereact/panel";
-import { Button } from "primereact/button";
-import { DataView } from "primereact/dataview";
-import { Dialog } from "primereact/dialog";
-import styles from "./ReviewPanel.module.css";
-import { Card } from "primereact/card";
-import { Dropdown } from "primereact/dropdown";
-import { Toast } from "primereact/toast";
-import CreateReviewForm from "../CreateReviewForm/CreateReviewForm";
-import { createReview, deleteReview, updateReview } from "@/app/api/review/route";
-import EditReviewForm from "../EditReviewForm/EditReviewForm";
-import Enums from "@/common/enums/enums";
-import { TabView, TabPanel } from "primereact/tabview";
-import { Image } from "primereact/image";
+import React, { useState, useRef } from 'react';
+import { Button } from 'primereact/button';
+import { DataView } from 'primereact/dataview';
+import { Dialog } from 'primereact/dialog';
+import styles from './ReviewPanel.module.css';
+import { Card } from 'primereact/card';
+import { Toast } from 'primereact/toast';
+import CreateReviewForm from '../CreateReviewForm/CreateReviewForm';
+import {
+  createReview,
+  deleteReview,
+  updateReview,
+} from '@/app/api/review/route';
+import EditReviewForm from '../EditReviewForm/EditReviewForm';
+import Enums from '@/common/enums/enums';
+import { TabView, TabPanel } from 'primereact/tabview';
+import { Image } from 'primereact/image';
 
 const ReviewPanel = ({
   formData,
@@ -35,31 +37,31 @@ const ReviewPanel = ({
   const [selectedReviewData, setSelectedReviewData] = useState(null);
 
   const formatDate = (dateString) => {
-    const options = { year: "numeric", month: "2-digit", day: "2-digit" };
-    return new Date(dateString).toLocaleDateString("en-GB", options);
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return new Date(dateString).toLocaleDateString('en-GB', options);
   };
 
   const receivedReviews = review.filter((r) =>
     roleRef === Enums.CORPORATE
-      ? r.reviewType === "Corporate"
-      : r.reviewType === "Job_Seeker"
+      ? r.reviewType === 'Corporate'
+      : r.reviewType === 'Job_Seeker'
   );
 
   const givenReviews = review.filter((r) =>
     roleRef === Enums.CORPORATE
-      ? r.reviewType === "Job_Seeker"
-      : r.reviewType === "Corporate"
+      ? r.reviewType === 'Job_Seeker'
+      : r.reviewType === 'Corporate'
   );
 
   const initialFormData = {
-    reviewId: "",
-    jobSeekerId: "",
-    corporateId: "",
-    description: "",
-    startDate: "",
-    endDate: "",
-    reviewType: "",
-    attitudeJS: "",
+    reviewId: '',
+    jobSeekerId: '',
+    corporateId: '',
+    description: '',
+    startDate: '',
+    endDate: '',
+    reviewType: '',
+    attitudeJS: '',
     professionalismJS: 0,
     passionJS: 0,
     benefitsCP: 0,
@@ -73,9 +75,9 @@ const ReviewPanel = ({
     <div
       className={styles.jobExperienceDiv}
       style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
       }}
     >
       <h2 className={styles.headerTitle}>My Reviews</h2>
@@ -150,13 +152,13 @@ const ReviewPanel = ({
 
     const role = roleRef;
 
-    console.log("Hello");
+    console.log('Hello');
 
     if (Object.keys(formErrors).length > 0) {
       toast.current.show({
-        severity: "warn",
-        summary: "Warning",
-        detail: "Please fix the form errors before submitting.",
+        severity: 'warn',
+        summary: 'Warning',
+        detail: 'Please fix the form errors before submitting.',
         life: 5000,
       });
       return;
@@ -164,7 +166,7 @@ const ReviewPanel = ({
 
     let reqBody;
 
-    if (formData.endDate === "null") {
+    if (formData.endDate === 'null') {
       reqBody = {
         startDate: formData.startDate,
         description: formData.description,
@@ -179,33 +181,33 @@ const ReviewPanel = ({
       };
     }
 
-    if(roleRef === Enums.CORPORATE) {
+    if (roleRef === Enums.CORPORATE) {
       reqBody.jobSeekerId = formData.jobSeekerId;
       reqBody.corporateId = userId;
-      reqBody.reviewType = "Job_Seeker"
+      reqBody.reviewType = 'Job_Seeker';
     } else if (roleRef === Enums.JOBSEEKER) {
       reqBody.jobSeekerId = userId;
       reqBody.corporateId = formData.corporateId;
-      reqBody.reviewType = "Corporate"
+      reqBody.reviewType = 'Corporate';
     }
 
     console.log(reqBody);
     try {
       const response = await createReview(reqBody, roleRef, sessionTokenRef);
       if (!response.error) {
-        console.log("Review created successfully:", response);
+        console.log('Review created successfully:', response);
         toast.current.show({
-          severity: "success",
-          summary: "Success",
-          detail: "Review created successfully!",
+          severity: 'success',
+          summary: 'Success',
+          detail: 'Review created successfully!',
           life: 5000,
         });
         setRefreshData((prev) => !prev);
       }
     } catch (error) {
       toast.current.show({
-        severity: "error",
-        summary: "Error",
+        severity: 'error',
+        summary: 'Error',
         detail: error.message,
         life: 5000,
       });
@@ -222,9 +224,9 @@ const ReviewPanel = ({
       // There are validation errors
       //alert("Please fix the form errors before submitting.");
       toast.current.show({
-        severity: "warn",
-        summary: "Warning",
-        detail: "Please fix the form errors before submitting.",
+        severity: 'warn',
+        summary: 'Warning',
+        detail: 'Please fix the form errors before submitting.',
         life: 5000,
       });
       return;
@@ -232,7 +234,7 @@ const ReviewPanel = ({
 
     let reqBody;
 
-    if (formData.endDate === "null") {
+    if (formData.endDate === 'null') {
       reqBody = {
         startDate: formData.startDate,
         description: formData.description,
@@ -246,25 +248,21 @@ const ReviewPanel = ({
     }
 
     try {
-      const response = await updateReview(
-        reviewId,
-        reqBody,
-        sessionTokenRef
-      );
+      const response = await updateReview(reviewId, reqBody, sessionTokenRef);
       if (!response.error) {
-        console.log("Review updated successfully:", response);
+        console.log('Review updated successfully:', response);
         toast.current.show({
-          severity: "success",
-          summary: "Success",
-          detail: "Review updated successfully!",
+          severity: 'success',
+          summary: 'Success',
+          detail: 'Review updated successfully!',
           life: 5000,
         });
         setRefreshData((prev) => !prev);
       }
     } catch (error) {
       toast.current.show({
-        severity: "error",
-        summary: "Error",
+        severity: 'error',
+        summary: 'Error',
         detail: error.message,
         life: 5000,
       });
@@ -276,21 +274,21 @@ const ReviewPanel = ({
 
   const removeReview = async (reviewId) => {
     try {
-      const response = await deleteReview(reviewId,sessionTokenRef);
-      console.log("Review is deleted", response);
+      const response = await deleteReview(reviewId, sessionTokenRef);
+      console.log('Review is deleted', response);
       toast.current.show({
-        severity: "success",
-        summary: "Success",
-        detail: "Deleted review successfully!",
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Deleted review successfully!',
         life: 5000,
       });
       setRefreshData((prev) => !prev);
     } catch (error) {
-      console.error("Error deleting review:", error);
+      console.error('Error deleting review:', error);
       //alert("There was an error deleting the job experience:");
       toast.current.show({
-        severity: "error",
-        summary: "Error",
+        severity: 'error',
+        summary: 'Error',
         detail: error.message,
         life: 5000,
       });
@@ -308,7 +306,7 @@ const ReviewPanel = ({
             {roleRef === Enums.JOBSEEKER && (
               <>
                 <div className={styles.imageContainer}>
-                  {review.corporate.profilePictureUrl !== "" ? (
+                  {review.corporate.profilePictureUrl !== '' ? (
                     <img
                       alt={review.corporate.profilePictureUrl}
                       src={review.corporate.profilePictureUrl}
@@ -337,7 +335,7 @@ const ReviewPanel = ({
             {roleRef === Enums.CORPORATE && (
               <>
                 <div className={styles.imageContainer}>
-                  {review.jobSeeker.profilePictureUrl !== "" ? (
+                  {review.jobSeeker.profilePictureUrl !== '' ? (
                     <img
                       alt={review.jobSeeker.profilePictureUrl}
                       src={review.jobSeeker.profilePictureUrl}
@@ -368,8 +366,8 @@ const ReviewPanel = ({
             <h4>{formatDate(review.startDate)}</h4>
             <h4 className={styles.hideOnMobile}>-</h4>
             <h4>
-              {formatDate(review.endDate) === "01/01/1970"
-                ? "Present"
+              {formatDate(review.endDate) === '01/01/1970'
+                ? 'Present'
                 : formatDate(review.endDate)}
             </h4>
           </div>
@@ -378,7 +376,7 @@ const ReviewPanel = ({
           <p>{review.description}</p>
         </div>
         <div className={styles.cardFooter}>
-          {review.reviewType === "Corporate" && roleRef === Enums.JOBSEEKER && (
+          {review.reviewType === 'Corporate' && roleRef === Enums.JOBSEEKER && (
             <>
               <Button
                 label="Edit"
@@ -402,7 +400,7 @@ const ReviewPanel = ({
               />
             </>
           )}
-          {review.reviewType === "Job_Seeker" &&
+          {review.reviewType === 'Job_Seeker' &&
             roleRef === Enums.CORPORATE && (
               <>
                 <Button
@@ -515,7 +513,7 @@ const ReviewPanel = ({
         footer={deleteDialogFooter}
       >
         <h3>
-          Confirm Delete Review:{" "}
+          Confirm Delete Review:{' '}
           {showDeleteReviewDialog && showDeleteReviewDialog.reviewId},
         </h3>
       </Dialog>
