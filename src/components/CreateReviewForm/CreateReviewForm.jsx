c291dc5313b4d956f065fa76114981ed6b749b04
Shawn Lim <e0727020@u.nus.edu>
2023-11-17 16:50:56 +0800

    edited styling for review form

--- src/components/CreateReviewForm/CreateReviewForm.jsx
+++ src/components/CreateReviewForm/CreateReviewForm.jsx
@@ -1,14 +1,11 @@
-import React, { useState, useEffect } from "react";
-import styles from "./CreateReviewForm.module.css";
-import { Card } from "primereact/card";
-import { InputText } from "primereact/inputtext";
-import { InputNumber } from "primereact/inputnumber";
-import { InputTextarea } from "primereact/inputtextarea";
-import { Calendar } from "primereact/calendar";
-import { Button } from "primereact/button";
-import { Checkbox } from "primereact/checkbox";
-import Enums from "@/common/enums/enums"; 
-import { Dropdown } from "primereact/dropdown";
+import React, { useState, useEffect } from 'react';
+import { Button } from 'primereact/button';
+import { Calendar } from 'primereact/calendar';
+import { Checkbox } from 'primereact/checkbox';
+import { Dropdown } from 'primereact/dropdown';
+import { InputTextarea } from 'primereact/inputtextarea';
+import Enums from '@/common/enums/enums';
+import styles from './CreateReviewForm.module.css';
 
 const CreateReviewForm = ({
   formData,
@@ -22,7 +19,7 @@ const CreateReviewForm = ({
 }) => {
   const [isCurrentJob, setIsCurrentJob] = useState(false);
 
-  const [selectedFilter, setSelectedFilter] = useState("");
+  const [selectedFilter, setSelectedFilter] = useState('');
   const [filterOptions, setFilterOptions] = useState([]);
 
   useEffect(() => {
@@ -54,204 +51,202 @@ const CreateReviewForm = ({
   }, [dropdownList, setFormData, selectedFilter]);
 
   return (
-    <div className={styles.container}>
-      <form className={styles.form} onSubmit={addReview}>
-        <div className={styles.cardBody}>
-          {roleRef === Enums.CORPORATE && (
-            <>
-              <div className={styles.cardRow}>
-                <label>Job Seeker username</label>
-                <Dropdown
-                  name="jobSeekerId"
-                  value={selectedFilter}
-                  options={filterOptions}
-                  onChange={(e) => setSelectedFilter(e.value)}
-                  placeholder="Select Job Seeker"
-                  required
-                />
-              </div>
+    <div className={styles.cardBody}>
+      <form onSubmit={addReview}>
+        {roleRef === Enums.CORPORATE && (
+          <>
+            <div className={styles.cardRow}>
+              <label>Job Seeker Name</label>
+              <Dropdown
+                name="jobSeekerId"
+                value={selectedFilter}
+                options={filterOptions}
+                onChange={(e) => setSelectedFilter(e.value)}
+                placeholder="Select Job Seeker"
+                required
+              />
+            </div>
 
-              <div className={styles.cardRow}>
-                <label>Start Date</label>
-                <Calendar
-                  id="startDate"
-                  name="startDate"
-                  value={formData?.startDate}
-                  dateFormat="dd/mm/yy"
-                  onChange={(e) =>
+            <div className={styles.cardRow}>
+              <label>Start Date</label>
+              <Calendar
+                id="startDate"
+                name="startDate"
+                value={formData?.startDate}
+                dateFormat="dd/mm/yy"
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    startDate: e.value,
+                  }))
+                }
+                required
+              />
+            </div>
+
+            {formErrors.endDate && (
+              <small className={styles.errorMessage}>
+                {formErrors.endDate}
+              </small>
+            )}
+
+            <div className={styles.cardRow}>
+              <label>Current Review</label>
+              <Checkbox
+                inputId="isCurrent"
+                checked={isCurrentJob}
+                onChange={(e) => {
+                  setIsCurrentJob(e.checked);
+                  if (e.checked) {
                     setFormData((prev) => ({
                       ...prev,
-                      startDate: e.value,
-                    }))
-                  }
-                  required
-                />
-              </div>
-
-              {formErrors.endDate && (
-                <small className={styles.errorMessage}>
-                  {formErrors.endDate}
-                </small>
-              )}
-
-              <div className={styles.cardRow}>
-                <label>Current Review</label>
-                <Checkbox
-                  inputId="isCurrent"
-                  checked={isCurrentJob}
-                  onChange={(e) => {
-                    setIsCurrentJob(e.checked);
-                    if (e.checked) {
-                      setFormData((prev) => ({
-                        ...prev,
-                        endDate: "null",
-                      }));
-                    }
-                  }}
-                />
-              </div>
-
-              <div className={styles.cardRow}>
-                <label>End Date</label>
-                <Calendar
-                  id="endDate"
-                  name="endDate"
-                  value={formData?.endDate}
-                  dateFormat="dd/mm/yy"
-                  disabled={isCurrentJob}
-                  maxDate={new Date(new Date().setDate(new Date().getDate()))}
-                  onChange={(e) => {
-                    if (e.value <= formData.startDate) {
-                      setFormErrors((prev) => ({
-                        ...prev,
-                        endDate: "End date must be after start date.",
-                      }));
-                    } else {
-                      setFormErrors((prev) => {
-                        const { endDate, ...rest } = prev;
-                        return rest;
-                      });
-                    }
-                    setFormData((prev) => ({
-                      ...prev,
-                      endDate: e.value,
+                      endDate: 'null',
                     }));
-                  }}
-                />
-              </div>
-
-              <div className={styles.cardRow}>
-                <label htmlFor="description">Description:</label>
-                <InputTextarea
-                  id="description"
-                  name="description"
-                  value={formData?.description}
-                  onChange={handleInputChange}
-                  rows={7}
-                  autoResize={true}
-                  required
-                />
-              </div>
-            </>
-          )}
-          {roleRef === Enums.JOBSEEKER && (
-            <>
-              <div className={styles.cardRow}>
-                <label>Corporate username</label>
-                <Dropdown
-                  name="corporateId"
-                  value={selectedFilter}
-                  options={filterOptions}
-                  onChange={(e) => setSelectedFilter(e.value)}
-                  placeholder="Select Corporate"
-                  required
-                />
-              </div>
-
-              <div className={styles.cardRow}>
-                <label>Start Date</label>
-                <Calendar
-                  id="startDate"
-                  name="startDate"
-                  value={formData?.startDate}
-                  dateFormat="dd/mm/yy"
-                  onChange={(e) =>
-                    setFormData((prev) => ({
-                      ...prev,
-                      startDate: e.value,
-                    }))
                   }
-                  required
-                />
-              </div>
+                }}
+              />
+            </div>
 
-              {formErrors.endDate && (
-                <small className={styles.errorMessage}>
-                  {formErrors.endDate}
-                </small>
-              )}
+            <div className={styles.cardRow}>
+              <label>End Date</label>
+              <Calendar
+                id="endDate"
+                name="endDate"
+                value={formData?.endDate}
+                dateFormat="dd/mm/yy"
+                disabled={isCurrentJob}
+                maxDate={new Date(new Date().setDate(new Date().getDate()))}
+                onChange={(e) => {
+                  if (e.value <= formData.startDate) {
+                    setFormErrors((prev) => ({
+                      ...prev,
+                      endDate: 'End date must be after start date.',
+                    }));
+                  } else {
+                    setFormErrors((prev) => {
+                      const { endDate, ...rest } = prev;
+                      return rest;
+                    });
+                  }
+                  setFormData((prev) => ({
+                    ...prev,
+                    endDate: e.value,
+                  }));
+                }}
+              />
+            </div>
 
-              <div className={styles.cardRow}>
-                <label>Current Review</label>
-                <Checkbox
-                  inputId="isCurrent"
-                  checked={isCurrentJob}
-                  onChange={(e) => {
-                    setIsCurrentJob(e.checked);
-                    if (e.checked) {
-                      setFormData((prev) => ({
-                        ...prev,
-                        endDate: "null",
-                      }));
-                    }
-                  }}
-                />
-              </div>
+            <div className={styles.cardRow}>
+              <label htmlFor="description">Description:</label>
+              <InputTextarea
+                id="description"
+                name="description"
+                value={formData?.description}
+                onChange={handleInputChange}
+                rows={5}
+                autoResize={true}
+                required
+              />
+            </div>
+          </>
+        )}
+        {roleRef === Enums.JOBSEEKER && (
+          <>
+            <div className={styles.cardRow}>
+              <label>Corporate Name</label>
+              <Dropdown
+                name="corporateId"
+                value={selectedFilter}
+                options={filterOptions}
+                onChange={(e) => setSelectedFilter(e.value)}
+                placeholder="Select Corporate"
+                required
+              />
+            </div>
 
-              <div className={styles.cardRow}>
-                <label>End Date</label>
-                <Calendar
-                  id="endDate"
-                  name="endDate"
-                  value={formData?.endDate}
-                  dateFormat="dd/mm/yy"
-                  disabled={isCurrentJob}
-                  maxDate={new Date(new Date().setDate(new Date().getDate()))}
-                  onChange={(e) => {
-                    if (e.value <= formData.startDate) {
-                      setFormErrors((prev) => ({
-                        ...prev,
-                        endDate: "End date must be after start date.",
-                      }));
-                    } else {
-                      setFormErrors((prev) => {
-                        const { endDate, ...rest } = prev;
-                        return rest;
-                      });
-                    }
+            <div className={styles.cardRow}>
+              <label>Start Date</label>
+              <Calendar
+                id="startDate"
+                name="startDate"
+                value={formData?.startDate}
+                dateFormat="dd/mm/yy"
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    startDate: e.value,
+                  }))
+                }
+                required
+              />
+            </div>
+
+            {formErrors.endDate && (
+              <small className={styles.errorMessage}>
+                {formErrors.endDate}
+              </small>
+            )}
+
+            <div className={styles.cardRow}>
+              <label>Current Review</label>
+              <Checkbox
+                inputId="isCurrent"
+                checked={isCurrentJob}
+                onChange={(e) => {
+                  setIsCurrentJob(e.checked);
+                  if (e.checked) {
                     setFormData((prev) => ({
                       ...prev,
-                      endDate: e.value,
+                      endDate: 'null',
                     }));
-                  }}
-                />
-              </div>
+                  }
+                }}
+              />
+            </div>
 
-              <div className={styles.cardRow}>
-                <label htmlFor="description">Description:</label>
-                <InputTextarea
-                  id="description"
-                  name="description"
-                  value={formData?.description}
-                  onChange={handleInputChange}
-                  rows={7}
-                  autoResize={true}
-                  required
-                />
-              </div>
-            </>
-          )}
-        </div>
+            <div className={styles.cardRow}>
+              <label>End Date</label>
+              <Calendar
+                id="endDate"
+                name="endDate"
+                value={formData?.endDate}
+                dateFormat="dd/mm/yy"
+                disabled={isCurrentJob}
+                maxDate={new Date(new Date().setDate(new Date().getDate()))}
+                onChange={(e) => {
+                  if (e.value <= formData.startDate) {
+                    setFormErrors((prev) => ({
+                      ...prev,
+                      endDate: 'End date must be after start date.',
+                    }));
+                  } else {
+                    setFormErrors((prev) => {
+                      const { endDate, ...rest } = prev;
+                      return rest;
+                    });
+                  }
+                  setFormData((prev) => ({
+                    ...prev,
+                    endDate: e.value,
+                  }));
+                }}
+              />
+            </div>
+
+            <div className={styles.cardRow}>
+              <label htmlFor="description">Description:</label>
+              <InputTextarea
+                id="description"
+                name="description"
+                value={formData?.description}
+                onChange={handleInputChange}
+                rows={5}
+                autoResize={true}
+                required
+              />
+            </div>
+          </>
+        )}
 
         <div className={styles.buttonContainer}>
           <Button label="Create Review" severity="success" raised />
